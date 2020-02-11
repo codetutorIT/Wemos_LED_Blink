313e858003127c8d8e64c28337e7b478ade4c477
schneiderEDU <cschneider@bbs-betzdorf-kirchen.de>
2020-02-11 15:56:45 +0100

    Added code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
 
+const unsigned int LED_PIN = D1;
+const unsigned int TIME_WAIT = 1000;
+
+unsigned long millis_current;
+unsigned long millis_previous = 0;
+
+bool isOn;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  isOn = false;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  millis_current = millis();
+  if(millis_current - millis_previous >= TIME_WAIT) {
+    Serial.print("Schalte LED ");
+    if(isOn) {
+      Serial.println("aus.");
+      digitalWrite(LED_PIN, LOW);      
+    } 
+    else {
+      Serial.println("ein.");
+      digitalWrite(LED_PIN, HIGH); 
+    }
+    isOn = !isOn;
+    millis_previous = millis_current;
+  }
 }
